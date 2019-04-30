84d7aad6e398dd0a789681533380d93e85d3b5fc
Emanuele Vivoli <emanuelevivoli@Emanueles-MacBook-Air.local>
2019-04-30 12:29:05 +0200

    good coded; divide in function;

--- preprocessing.py
+++ preprocessing.py
@@ -20,158 +20,233 @@ path_ell = './a_ellipses/'
 paths = os.listdir(path_in)
 
 colours = [
-            (0, 0, 255),        # blue
-            (0,128,0),	        # green
-            (173,255,47),       # greenyellow	
-            (255, 160, 0),      # orange
-            (0,238,0),	        # green2
-            (250, 246, 0),      # yellow
-            (0,205,0),          # green3
-            (255, 186, 230),    # pink
-            (255, 69, 230),     # purple
-            (0,255,0),	        # green1	 
-            (0,139,0),          # green4	
+            [ (0, 0, 255)    ,   'blue'        ],
+            [ (0,128,0)      ,   'green'       ],
+            [ (173,255,47)   ,   'greenyellow' ],
+            [ (255, 160, 0)  ,   'orange'      ],
+            [ (0,238,0)      ,	'green2'       ],
+            [ (250, 246, 0)  ,   'yellow'      ],
+            [ (0,205,0)      ,   'green3'      ],
+            [ (255, 186, 230),   'pink'        ],
+            [ (255, 69, 230) ,   'purple'      ],
+            [ (0,255,0)      ,	'green1'       ],	 
+            [ (0,139,0)      ,   'green4'      ]	
 ]
 
+def otsu_grid(img_grey, grid):
+    """ 
 
-def preprocessing(name_img):
+        GOAL:   the funciton create grid x grid cells,
+                and apply otsu on each of this cells.
+                Then put all sub-mask in one.
 
-    img_natural = cv2.imread(path_in + name_img, cv2.IMREAD_GRAYSCALE)
+        PARAMS:
+            (input)
+                - img_grey: 
+                    original grey scale image
+                - grid: 
+                    number of cell for side
 
-    img_median = cv2.medianBlur(img_natural,3)
+            (output)
+                - img_otsu:
+                    result mask image
 
-    img_end = img_median.copy()
-    grid = 2
+    """
+    img_otsu = img_grey.copy()
+
     for i in range(grid):
         for j in range(grid):
-            th, im = cv2.threshold(np.array([ np.array(row[j*int(len(img_median[0])/grid):(j+1)*int(len(img_median[0])/grid)]) for row in img_median[i*int(len(img_median)/grid):(i+1)*int(len(img_median)/grid)]]), 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+            # threshold and image of one single cell
+            th, im = cv2.threshold(np.array([ np.array(row[j*int(len(img_grey[0])/grid):(j+1)*int(len(img_grey[0])/grid)]) for row in img_grey[i*int(len(img_grey)/grid):(i+1)*int(len(img_grey)/grid)]]), 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
+            # unify image cell in one unique image, cell by cell
             for k in range(len(im)):
                 for y in range(len(im[0])):
-                    img_end[i*int(len(img_median)/grid)+k][j*int(len(img_median[0])/grid)+y] = im[k][y]
+                    img_otsu[i*int(len(img_grey)/grid)+k][j*int(len(img_grey[0])/grid)+y] = im[k][y]
 
-    (threshold_value, img_binary) = cv2.threshold(img_end, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-
+    return img_otsu
     
 
-    # kernel = diamond(10)
-    kernel = rectangle(16, 6)
-    img_dilation = dilation(img_binary, kernel)
-    kernel = square(9)
-    img_erosion = erosion(img_dilation, kernel)
+def preprocessing(img_grey):
+    """ 
 
-    # img_gaussian_blur = cv2.GaussianBlur(img_erosion,(5,5),0)
-    img_binary = img_erosion.copy()
-    # Elliptical Kernel
+        GOAL:   the funciton make the preprocessing applying
+                some filter, dilation, erosion and closure.
+
+        PARAMS:
+            (input)
+                - img_grey: 
+                    original grey scale image
+
+            (output)
+                - img_binary:
+                    result binary image
+
+    """
+
+    # apply median blurr to delete sale and pepper noise
+    img_median = cv2.medianBlur(img_grey,3)
+    
+    # grid = 2 means:  2 x 2 cells
+    grid = 2   
+
+    # get mask image
+    img_binary = otsu_grid(img_median, grid)
+
+    # dilation with rectangle of dimension 6 x 16 
+    kernel = rectangle(16, 6)
+    img_binary = dilation(img_binary, kernel)
+
+    # erosion with square of dimension 9 x 9 
+    kernel = square(9)
+    img_binary = erosion(img_binary, kernel)
+
+    # closure with elliptical kernel of dimension 16 x 16
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(16, 16))
     img_binary = cv2.morphologyEx(img_binary, cv2.MORPH_CLOSE, kernel)
 
-    # find center
-    # contours_end, hierarchy_end = cv2.findContours(img_end, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return img_binary
 
-    # create an empty black image
-    drawing_end = np.zeros((img_end.shape[0], img_end.shape[1]), np.uint8)
-    # draw contours and hull points
-    # for i in range(len(contours_end)):
-    #     print("contour ", i, " color: ", colours[i%len(colours)], " area: ", cv2.contourArea(contours_end[i]))
-    #     color_contours_end = colours[i%len(colours)] #(0, 255, 0) # green - color for contours
-    #     color = (255, 0, 0) # blue - color for convex hull
-    #     # draw ith contour
-        
-    #     cv2.drawContours(drawing_end, contours_end, i, color_contours_end, 1, 8, hierarchy_end)
-        
-    #     # draw ith convex hull object
-    #     # cv2.drawContours(drawing, hull, i, color, 1, 8)
-    
-    # find center
+
+def getOneHandContour(img_binary):
+    """ 
+
+        GOAL:   the funciton calculates the contours of image,
+                and take only the hand contour (the one that has the biggest area)
+
+        PARAMS:
+            (input)
+                - img_binary: 
+                    original binary image
+
+            (output)
+                - contour:
+                    result hand contour
+
+    """
+
+    # find contours of processed image
     contours_bin, hierarchy_bin = cv2.findContours(img_binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # create an empty black image
-    # draw contours and hull points
+    # we have more than one contours in some image
+    # than we need to consider only hand contour, it is 
+    # the biggest one
     area_max = 0
     index_area_max = -1
+
+    # select biggest contour
     for i in range(len(contours_bin)):
-        print("contour ", i, " color: ", colours[i%len(colours)], " area: ", cv2.contourArea(contours_bin[i]))
-        color_contours_bin = colours[i%len(colours)] #(0, 255, 0) # green - color for contours
-        color = (255, 0, 0) # blue - color for convex hull
-        # draw ith contour
+
+        # get area of ith contour
         area_i = cv2.contourArea(contours_bin[i])
+
+        # check if dimension of area is bigger than the biggest
         if area_max < area_i:
             index_area_max = i
             area_max = area_i
 
-        # cv2.drawContours(drawing_end, contours_bin, i, color_contours_bin, 1, 8, hierarchy_bin)
-        
-        # draw ith convex hull object
-        # cv2.drawContours(drawing, hull, i, color, 1, 8)
+    return contours_bin[index_area_max]
 
-    contours_bin = contours_bin[index_area_max]
 
-    cv2.fillPoly(drawing_end, pts =[contours_bin], color=(255))
+def getHand(img_grey):
+    """ 
+
+        GOAL:   the funciton calculate on grey scale input image
+                a mask of the hand, and return the mask with also ellipse 
+                and some important params of ellipse.
+
+        PARAMS:
+            (input)
+                - img_grey: 
+                    the original gery scale image
+
+            (output)
+                - hand_mask: 
+                    the mask of the hand
+                - prop: 
+                    array with center of mass, length of major and 
+                    minor axes of ellipse, and orientation of ellipse.
+                - ellipse_mask: 
+                    same image of hand_mask with drawn on the ellipse
+                    and the axes
+
+
+    """
+
+    # get binary image after preprocessing
+    img_binary = preprocessing(img_grey)
+
+    # create an empty black image
+    hand_mask = np.zeros((img_binary.shape[0], img_binary.shape[1]), np.uint8)
     
-    #cv2.drawContours(drawing_end, contours_bin, -1, 255, 1)
-    
-    cv2.imwrite(path_out + name_img, drawing_end)
+    contour = getOneHandContour(img_binary)
 
-    img_binary = drawing_end.copy()
+    # create polylines from contour points, and draw if filled in image
+    cv2.fillPoly(hand_mask, pts =[contour], color=(255))
 
+    # get a copy of image in order to work with
+    img_binary = hand_mask.copy()
+
+    # create a binary mask, where img is white(255) put 1, else let 0
     img_binary[img_binary == 255] = 1
-    
-    labeled_foreground = (img_binary > 0).astype(int)
-    # print(labeled_foreground.shape, img_binary.shape)
 
+    # create a labeled mask of image where image is 1
+    labeled_foreground = (img_binary > 0).astype(int)
+
+    # skimage regionprops need labeled mask and binary mask
+    # and return properties object with pixels property like
+    # centroid, ellipse around pixels, ecc...
     properties = regionprops(labeled_foreground, img_binary, coordinates='xy')
 
+    # get center of mass of pixels (also called centroid)
     center_of_mass = properties[0].centroid
 
+    # get integer values of center of mass
+    y0, x0 = center_of_mass
+    y0, x0 = int(y0), int(x0)
+
+    # exists also weighted center of mass
     weighted_center_of_mass = properties[0].weighted_centroid
 
-    major_axis_lenght = properties[0].major_axis_length
+    # get major and minor axis of ellips
+    major_axis_length = properties[0].major_axis_length
+    minor_axis_length = properties[0].minor_axis_length
+
+    # get orientation of ellipse (in degree from x axis) 
     orientation = properties[0].orientation
     
     print('\n \n')
-    print('centroid coord:',center_of_mass)
-    print('maj ax lenght: ', major_axis_lenght)
-    print('min ax lenght: ', properties[0].minor_axis_length)
-    print('orientation : ', orientation)
+    print('centroid coord: ' , center_of_mass)
+    print('maj ax lenght:  ' , major_axis_length)
+    print('min ax lenght:  ' , minor_axis_length)
+    print('orientation :   ' , orientation)
 
-#   ----------------------------------------------------------------------------    #
+    prop = [center_of_mass, major_axis_length, minor_axis_length, orientation]
 
-
-    # draw img with center of mass / centroid 
-    colorized = label2rgb(labeled_foreground, img_binary, colors=['red', 'white'], alpha=0.2)
-    colorized = 255*colorized
+    # create image from binary image and labeled mask
+    ellipse_mask = label2rgb(labeled_foreground, img_binary, colors=['red', 'white'], alpha=0.2)
+    ellipse_mask = 255 * ellipse_mask
     
-    # cv2.imwrite(path_color_out + name_img, colorized)
+    # draw in that image the center of mass and weighted center of mass
+    cv2.circle(ellipse_mask,(x0, y0), 5, (0,0,255), -1)
+    cv2.circle(ellipse_mask,(int(weighted_center_of_mass[1]), int(weighted_center_of_mass[0])), 5, (0,255,255), -1)
 
-    # ax.plot(weighted_center_of_mass[1], weighted_center_of_mass[0], marker = 'o', color= 'purple'
-    # )
-    cv2.circle(colorized,(int(weighted_center_of_mass[1]), int(weighted_center_of_mass[0])), 5, (0,255,255), -1)
+    # calculate and draw major axis of ellipse
+    x1 = int(x0 + math.cos(orientation) * 0.5 * major_axis_length)
+    y1 = int(y0 - math.sin(orientation) * 0.5 * major_axis_length)
+    cv2.line(ellipse_mask,(x0, y0), (x1, y1), (255,0,0) , 3) 
 
-    # ax.scatter(center_of_mass[1], center_of_mass[0], s=160, c='C0', marker='+')
-    cv2.circle(colorized,(int(center_of_mass[1]), int(center_of_mass[0])), 5, (0,0,255), -1)
+    # calculate and draw minor axis of ellipse
+    x2 = int(x0 - math.sin(orientation) * 0.5 * minor_axis_length)
+    y2 = int(y0 - math.cos(orientation) * 0.5 * minor_axis_length)
+    cv2.line(ellipse_mask,(x0, y0), (x2, y2), (255,0,0) , 3) 
 
-    x2, y2 = [(int(center_of_mass[1]), int(center_of_mass[0]))], [0, center_of_mass[0]*1.9]
-    # ax.plot(x2, y2, marker = 'o', color= 'green')
-
-    y0, x0 = properties[0].centroid
-    y0, x0 = int(y0), int(x0)
-    orientation = properties[0].orientation
-    x1 = int(x0 + math.cos(orientation) * 0.5 * properties[0].major_axis_length)
-    y1 = int(y0 - math.sin(orientation) * 0.5 * properties[0].major_axis_length)
-    # ax.plot((x0, x1), (y0, y1), '-r', linewidth=2.5, color = 'red')
-    cv2.line(colorized,(x0, y0), (x1, y1), (255,0,0) , 3) 
-
-    x2 = int(x0 - math.sin(orientation) * 0.5 * properties[0].minor_axis_length)
-    y2 = int(y0 - math.cos(orientation) * 0.5 * properties[0].minor_axis_length)
-    # ax.plot((x0, x2), (y0, y2), '-r', linewidth=2.5, color = 'red')
-    cv2.line(colorized,(x0, y0), (x2, y2), (255,0,0) , 3) 
-
-    mean = [ x0,y0 ]
-    width = int(properties[0].major_axis_length/2)
-    height = int(properties[0].minor_axis_length/2)
+    # set width and height for ellipse
+    width = int(major_axis_length/2)
+    height = int(minor_axis_length/2)
     
-    cv2.ellipse(colorized,
+    # draw ellipse 
+    cv2.ellipse(ellipse_mask,
                 (x0,y0),
                 (height, width), 
                 int(90 - orientation*360/(2*np.pi)), 
@@ -180,27 +255,23 @@ def preprocessing(name_img):
                 color=255, 
                 thickness=2)
     
-    # , int(90 - orientation*360/(2*np.pi)),0, 360, color=tuple(255),  thickness=1)
-    # ell = mpl.patches.Ellipse(xy=mean, width=height, height=width, angle = 90 - orientation*360/(2*np.pi), color = 'red', edgecolor='r', fc='None', lw=2)
-
-    # ax.add_patch(ell)
-    # ax.set_aspect('equal')
-    # ax.autoscale()
-
-    cv2.imwrite(path_ell + name_img, colorized)
-
-    # plt.savefig(path_convx_out + name_img)
-    # plt.show(path_convx_out + name_img)
+    return hand_mask, prop, ellipse_mask
     
 
 def main():
+    for name_img in paths:
+        # read image in a grey scale way
+        img_grey = cv2.imread(path_in + name_img, cv2.IMREAD_GRAYSCALE)
 
-    for path in paths:
-        preprocessing(path)
+        # apply the preprocessing to the grey scale image
+        hand_mask, _ , ellipse_mask = getHand(img_grey)
 
-# preprocessing('img_dataset2.png')
+        # save image in output path    
+        cv2.imwrite(path_out + name_img, hand_mask)
 
-
+        # save image with mask background and axes and ellipse
+        cv2.imwrite(path_ell + name_img, ellipse_mask)
+        
 
 if __name__== "__main__":
   main()
